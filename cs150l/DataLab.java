67a16d02886a46f9ff0962d75f2c27728e84477c
Hadrian <hadrian.lazic@gmail.com>
2026-09-16 18:45:50 -0700

    capped register arguments to byte from a int applied bitmask.

--- cs150l/DataLab.java
+++ cs150l/DataLab.java
@@ -73,12 +73,14 @@ public class DataLab {
       XOR, // 15
     }
 
-    double[] registers = { 0, 0, 0, 0 };
+    double[] registers = { 0, 0, 0, 0, 0, 0 };
 
-    for (int i = 0; i > 256; i++) {
+    for (int i = 0; i < 64; i++) {
       int maskedOpcode = i & 0b00001111;
-      int maskedArg0 = i & 0b11000000;
-      int maskedArg1 = i & 0b00110000;
+      int maskedArg0 = (byte) i & 0b11000000;
+      int maskedArg1 = (byte) i & 0b00110000;
+
+      System.out.println("" + registers);
 
       switch (maskedOpcode) {
         case 0: {
